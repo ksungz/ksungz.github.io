1988eefd5720fac0aafb7b5da95d3374c7a64852
Sungjae_Kim <k.suzkim@gmail.com>
2025-09-30 01:47:21 +0900

    [] - readme update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/base/global.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>

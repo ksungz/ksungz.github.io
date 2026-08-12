cd918d04e9ec153ed9c01726b8a37e6892777b58
Sungjae_Kim <24910323+ksungz@users.noreply.github.com>
2026-08-13 02:09:52 +0900

    Add React practice challenges

--- public/learning/assets/ChallengeListPage-BRIAZvVn.js
+++
@@ -1,1 +0,0 @@
-import{j as e,N as t}from"./index-Btraqu8b.js";import{f as a}from"./registry-CRfzjDOf.js";const i=()=>e.jsx(t,{to:`/ide/${a.id}`,replace:!0});export{i as ChallengeListPage};
---
+++ public/learning/assets/ChallengeListPage-wthhDP8E.js
@@ -0,0 +1,1 @@
+import{j as e,N as t}from"./index-RP8fdBN-.js";import{f as a}from"./registry-XPTikWxf.js";const i=()=>e.jsx(t,{to:`/ide/${a.id}`,replace:!0});export{i as ChallengeListPage};
